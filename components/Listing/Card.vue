c8591fda45e8681e47f836d7524976f28eb9ba5a
sv022 <ssvatkin04@gmail.com>
2024-04-28 20:06:53 +0300

    Added listings display components

---
+++ components/Listing/Card.vue
@@ -0,0 +1,54 @@
+<script setup>
+
+defineProps(["item"])
+
+</script>
+
+<template>
+
+<div class="listing">
+    <div style="text-align: center; width: 100%;">
+        <img src="/img/listing_placeholder_2.jpg" class="listing_photo">
+    </div>
+    <div>
+        <h5>{{ item.name }}</h5>
+        <div class="h-8 flex">
+            <h4>{{ item.price }} ₽</h4>
+            <img class="heart_listing">
+        </div>
+        <h6>LOCATION</h6>
+    </div>
+</div>        
+    
+</template>
+
+<style scoped>
+
+.listing {
+  height: 350px;
+  width: 300px;
+  padding: 0.5%;
+  /* border: 1px solid #f3f3f3; */
+  border-radius: 10px;
+  margin: 3px;
+  color: #232122;
+}
+.listing:hover {
+  box-shadow: 3px 3px rgb(201, 208, 209);
+  box-shadow: 3px 3px 6px rgb(238, 244, 245), -3px -3px 6px rgb(238, 244, 245), -3px 3px 6px rgb(238, 244, 245), 3px -3px 6px rgb(238, 244, 245);
+}
+.listing_photo {
+  width: 270px;
+  height: 200px;
+  align-content: center;
+  border-radius: 10px;
+  object-fit: cover;
+}
+.heart_listing {
+  content: url("img/heart_outline_black.png");
+  height: 25px; 
+  padding: 2%;
+  margin-left: auto;
+}
+
+</style>
---
+++ components/Listing/Container.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+let l = [{"id": 1, "name": "1", "price": 10000}, {"id": 2, "name": "2", "price": 20000}, {"id": 3, "name": "3", "price": 30000}, 
+{"id": 4, "name": "4", "price": 40000}, {"id": 5, "name": "5", "price": 50000}, {"id": 6, "name": "6", "price": 60000}, 
+{"id": 7, "name": "7", "price": 70000}, {"id": 8, "name": "8", "price": 80000}, {"id": 9, "name": "9", "price": 90000}, ]
+
+let listings = ref(l)
+
+
+</script>
+
+<template>
+    <div class="flex flex-wrap p-10">
+        <ListingCard v-for="listing in listings" :key="listing.id" :item="listing"/>
+    </div>
+</template>
