47b0b0f7e04e1e176f7b437a3a30a47592795656
sv022 <ssvatkin04@gmail.com>
2024-04-28 20:08:01 +0300

    Update color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        "lime-green": "#a5c05b",
+        "lime-green":{ 
+          100: "#a5c05b", 
+          200: "#65ae6c", 
+          300: "#48a366", 
+          400: "#2d967a", 
+        },
+        "red": "#fe6c5f",
+        "red-hover": "#f73b3b",
         "cream-white": "#eee8d9",
         "cold-white": "#f8fff8",
         "slate-gray": "#a9ad9b",
-        "dark-blue": "#2f4858",
-        "blueish": "#7BA4A8"
+        "soft-gray": "#dddede",
+        "blue-gray": "#7BA4A8",
+        "dark-blue": "#2f4858"
       }
     },
   },
