a47686bf33fd7093bcdfcb5f6b7c5a1c1c89775b
sv022 <ssvatkin04@gmail.com>
2024-05-04 19:15:02 +0300

    Added listing page

---
+++ components/Listing/Page/Container.vue
@@ -0,0 +1,11 @@
+<template>
+
+<div class="p-5 md:p-16 pt-5">
+    <div class="flex flex-col md:flex-row">
+        <ListingPageImage />
+        <ListingPageInfo />
+    </div>
+    <ListingPageDescription/>
+</div>        
+
+</template>
---
+++ components/Listing/Page/Description.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="px-3">
+        <h3 class="text-3xl p-4">Описание</h3>
+        <p class="w-full md:w-5/6">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, nulla veniam similique, optio recusandae quia repudiandae, eaque aperiam odit eveniet ipsam voluptatibus? Aliquid ipsa eligendi est esse optio a modi!
+        </p>
+    </div> 
+</template>
