bc33deb31d2f700f68fbd97c4bb7d7fde4199f9f
sv022 <ssvatkin04@gmail.com>
2024-05-04 19:15:17 +0300

    Styling fixes (forgor)

--- components/Listing/Card.vue
+++ components/Listing/Card.vue
@@ -1,12 +1,16 @@
 <script setup>
 
-defineProps(["item"])
+const props = defineProps(["item"])
+
+function navigate(){
+  return navigateTo(`product/${props.item.id}`)
+}
 
 </script>
 
 <template>
 
-<div class="listing">
+<div class="listing" v-on:click="navigate()">
     <div style="text-align: center; width: 100%;">
         <img src="/img/listing_placeholder_2.jpg" class="listing_photo">
     </div>
@@ -16,10 +20,6 @@ defineProps(["item"])
             <h4>{{ item.price }} ₽</h4>
             <!-- heart icon -->
             <svg class="ml-auto mr-3 cursor-pointer" width="30.000000" height="30.000000" viewBox="0 0 33 30" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-              <desc>
-                  Created with Pixso.
-              </desc>
-              <defs/>
               <path d="M16.23 2.48C20.04 -0.95 25.93 -0.83 29.6 2.85C33.27 6.53 33.4 12.39 29.99 16.21L16.22 30L2.46 16.21C-0.95 12.39 -0.82 6.52 2.85 2.85C6.52 -0.83 12.4 -0.95 16.23 2.48L16.23 2.48ZM27.3 5.14C24.87 2.7 20.94 2.6 18.39 4.89L16.23 6.84L14.06 4.89C11.5 2.6 7.58 2.7 5.14 5.14C2.72 7.56 2.6 11.43 4.83 13.99L16.22 25.4L27.62 13.99C29.85 11.43 29.73 7.57 27.3 5.14L27.3 5.14Z" fill="#444444" fill-opacity="1.000000" fill-rule="evenodd"/>
             </svg>
             <!-- <img class="heart_listing"> -->
