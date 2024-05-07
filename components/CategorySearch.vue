1719a003ea18ab6170000e46cebaa9f6f9593274
sv022 <ssvatkin04@gmail.com>
2024-05-07 11:36:23 +0300

    Switch to tailwind styles

--- components/CategorySearch.vue
+++ components/CategorySearch.vue
@@ -2,11 +2,11 @@
     <div class="p-10">
         <h2 class="p-10 pl-0 text-3xl">Искать в категории</h2>
         <div class="flex flex-wrap w-3/4 pl-10">
-            <div class="category">Электрогитары</div>
-            <div class="category">Классические гитары</div>
-            <div class="category">Акустические гитары</div>
-            <div class="category">Гитарное усиление</div>
-            <div class="category">Педали эффектов</div>
+            <div class="w-56 border border-gray-900 p-2 m-1 text-nowrap text-center rounded">Электрогитары</div>
+            <div class="w-56 border border-gray-900 p-2 m-1 text-nowrap text-center rounded">Классические гитары</div>
+            <div class="w-56 border border-gray-900 p-2 m-1 text-nowrap text-center rounded">Акустические гитары</div>
+            <div class="w-56 border border-gray-900 p-2 m-1 text-nowrap text-center rounded">Гитарное усиление</div>
+            <div class="w-56 border border-gray-900 p-2 m-1 text-nowrap text-center rounded">Педали эффектов</div>
         </div>
     </div>
 </template>
