f19496c983f1d3d291c6df19bbd727d63b4c90a3
sv022 <ssvatkin04@gmail.com>
2024-05-07 09:58:25 +0300

    Added: Save user data to localstorage

--- stores/userStore.ts
+++ stores/userStore.ts
@@ -1,15 +1,16 @@
 import type { IUser, IUserLogin } from "~/types/user.interface";
 
 export const useUserStore = defineStore('UserStore', () => {
-    const isAuth = ref(false);
-    const user = ref({} as IUser);
-    const token = ref('');
-    const refreshToken = ref('');
+    const isAuth = ref(false || !(!(localStorage.getItem("_token"))));
+    const user = ref({username: "" || localStorage.getItem("_cachedUserData"), password: ""} as IUser);
+    const token = ref('' || localStorage.getItem("_token"));
+    const refreshToken = ref('' || "_tokenRefresh");
     const errorAuth = ref(false);
     const errorMsg = ref('');   
 
     const userSingUp = (userInfo : IUser) => {
         user.value = userInfo;
+        navigateTo('/signin');
     }
 
     const userLogin = (userInfo : IUserLogin) => {
@@ -19,7 +20,13 @@ export const useUserStore = defineStore('UserStore', () => {
             refreshToken.value = "refreshToken";
             errorAuth.value = false;
             errorMsg.value = "";
+
+            localStorage.setItem("_token", token.value);
+            localStorage.setItem("_tokenRefresh", refreshToken.value);
+            localStorage.setItem("_cachedUserData", userInfo.username);
+
             navigateTo('/');
+
         } else {
             errorAuth.value = true;
             errorMsg.value = "Неверно введены данные или аккаунта не существует";
@@ -31,6 +38,11 @@ export const useUserStore = defineStore('UserStore', () => {
         user.value = {} as IUser;
         token.value = "";
         refreshToken.value = "";
+
+        localStorage.setItem("_token", "");
+        localStorage.setItem("_tokenRefresh", "");
+        localStorage.setItem("_cachedUserData", "");
+
         navigateTo('/');
     }
 
