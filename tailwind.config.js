01ac0f98682509695c9c4b3ec679065d473e4603
sv022 <ssvatkin04@gmail.com>
2024-05-07 10:38:25 +0300

    Restore color pallet

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,6 +33,23 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      colors: {
+        "lime-green":{ 
+          100: "#a5c05b", 
+          200: "#65ae6c", 
+          300: "#48a366", 
+          400: "#2d967a", 
+        },
+        "red": "#fe6c5f",
+        "red-hover": "#f73b3b",
+        "cream-white": "#eee8d9",
+        "cold-white": "#f8fff8",
+        "slate-gray": "#a9ad9b",
+        "soft-gray": "#dddede",
+        //"blue-gray": "#7BA4A8",
+        "blue-gray": "#94b0b3",
+        "dark-blue": "#2f4858"
+      }
     },
   },
   plugins: [animate],
