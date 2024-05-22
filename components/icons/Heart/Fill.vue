a681d0b215d15b577ca98d2ad26d652c06765695
sv022 <ssvatkin04@gmail.com>
2024-05-22 17:31:23 +0300

    update: fix icons style

---
+++ components/icons/Heart/Fill.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <svg width="30.000000" height="28.000000" viewBox="0 0 30 28" fill="none" xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink">
+        <defs />
+        <path id="矢量 118"
+            d="M15 27.52L12.82 25.54C5.09 18.54 0 13.91 0 8.25C0 3.62 3.63 0 8.25 0C10.86 0 13.36 1.21 15 3.13C16.63 1.21 19.14 0 21.75 0C26.37 0 30 3.62 30 8.25C30 13.91 24.9 18.54 17.17 25.56L15 27.52Z"
+            fill="#D58C58" fill-opacity="1.000000" fill-rule="evenodd" />
+    </svg>
+
+</template>
+
+<style>
+svg {
+    cursor: pointer;
+}
+</style>
