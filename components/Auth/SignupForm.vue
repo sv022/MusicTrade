1c8eb9bbce87aa4813859366ccfed2fb94f05758
sv022 <ssvatkin04@gmail.com>
2024-05-31 15:34:42 +0300

    fix: jittery forms (appearing hints)

--- components/Auth/SignupForm.vue
+++ components/Auth/SignupForm.vue
@@ -31,20 +31,20 @@ const onSubmit = handleSubmit(userInfo => {
         <div class="mb-4">
             <label class="block text-gray-700 text-sm font-bold mb-2" for="username">Имя пользователя:</label>
             <input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" v-model="username" v-bind="usernameAttrs" placeholder="Имя пользователя">
-            <div class="text-xs text-red-hover">{{ errors.username }}</div>
+            <div class="text-xs text-red-hover min-h-4">{{ errors.username }}</div>
         </div>
         <div class="mb-4">
             <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email:</label>
             <input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" v-model="email" v-bind="emailAttrs" type="email" placeholder="Email">
-            <div class="text-xs text-red-hover">{{ errors.email }}</div>
+            <div class="text-xs text-red-hover min-h-4">{{ errors.email }}</div>
         </div>
         <div class="mb-4">
             <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Пароль:</label>
             <input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" v-model="password" v-bind="passwordAttrs" type="password" placeholder="Пароль">
-            <div class="text-xs text-red-hover">{{ errors.password }}</div>
+            <div class="text-xs text-red-hover min-h-4">{{ errors.password }}</div>
         </div>  
         <div class="flex items-center justify-between">
-            <button class="bg-lime-green-100 hover:bg-lime-green-200 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer">Зарегистрироваться</button>
+            <button class="bg-lime-green-100 hover:bg-lime-green-200 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline cursor-pointer">Cоздать аккаунт</button>
             <NuxtLink class="inline-block align-baseline font-bold text-sm text-red hover:text-red-hover" to='/signin'>Отмена</NuxtLink>
         </div>
     </form>
